bf3c396982a9d9058756b9de2d4592a93f371c4a
Tommi Lehtinen <tommijmlehtinen@gmail.com>
2024-09-02 18:18:39 +0300

    toka tunti

---
+++ public/script.js
@@ -0,0 +1,7 @@
+const messageButton = document.getElementById("message-button");
+
+messageButton.addEventListener("click", sendMessage);
+
+function sendMessage() {
+    console.log("nappia painettu");
+}
