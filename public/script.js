764ac45de48913bca87e650af51807d1b2c0e4bc
Tommi Lehtinen <tommijmlehtinen@gmail.com>
2024-09-09 18:18:30 +0300

    09092024 eli kolmas tunti

--- public/script.js
+++ public/script.js
@@ -1,7 +1,17 @@
 const messageButton = document.getElementById("message-button");
+const messageInput = document.getElementById("message-input");
+const chatbox = document.getElementById("chatbox");
 
 messageButton.addEventListener("click", sendMessage);
 
 function sendMessage() {
-    console.log("nappia painettu");
+    const userMessage = messageInput.value;
+    messageInput.value = "";
+    addMessageTo(chatbox, userMessage);
+}
+
+function addMessageTo(box, message) {
+    const messageElement = document.createElement("div");
+    messageElement.textContent = message;
+    box.appendChild(messageElement);
 }
